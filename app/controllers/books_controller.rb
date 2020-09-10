82d0b2d3e37e7fc304269dd62831d410a282c7b0
川本悠人 <eugenenzv8@gmail.com>
2020-09-10 20:33:58 +0900

    fix all

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,20 @@
 class BooksController < ApplicationController
-	before_action :authenticate_user!
-
-  def show
-    @book = Book.find(params[:id])
-  end
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @books = Book.all
     @book = Book.new
   end
 
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+  end
+
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id　#ここで、本の投稿者とログインユーザを一致させる＝誰が投稿したかという意味。
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -21,13 +24,9 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
-
-
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -35,8 +34,7 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
+  def destroy
     @book.destoy
     redirect_to books_path
   end
@@ -47,4 +45,10 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
+  end
 end
